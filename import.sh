7822d54e653cef4b603e7e77433bb6d62f7b8860
Nathan Rajlich <nathan@tootallnate.net>
2018-07-09 16:58:30 -0700

    Clean up tmp file after failed curl, and use `return`

--- import.sh
+++ import.sh
@@ -1,16 +1,20 @@
 import() {
   local hash
-  hash="$(echo "$1" | sha1sum | { read -r first rest; echo "$first"; })" || exit
+  hash="$(echo "$1" | sha1sum | { read -r first rest; echo "$first"; })" || return
   local cache="${IMPORT_CACHE-${HOME}/.import-cache}"
   local cachefile="${cache}/${hash}"
   if [ ! -f "${cachefile}" ] || [ ! -z "${IMPORT_RELOAD-}" ]; then
-    mkdir -p "${cache}" || exit
-    curl -fsSL --netrc-optional ${IMPORT_CURL_OPTS-} "$1" > "${cachefile}.tmp" || exit
-    mv "${cachefile}.tmp" "${cachefile}" || exit
+    mkdir -p "${cache}" || return
+    curl -fsSL --netrc-optional ${IMPORT_CURL_OPTS-} "$1" > "${cachefile}.tmp" || {
+      r=$?
+      rm "${cachefile}.tmp" || return
+      return "$r"
+    }
+    mv "${cachefile}.tmp" "${cachefile}" || return
     [ ! -z "${IMPORT_DEBUG-}" ] && echo "imported: $1" >&2
   fi
   if [ -z "${print-}" ]; then
-    source "${cachefile}" || exit
+    source "${cachefile}" || return
   else
     echo "${cachefile}"
   fi
