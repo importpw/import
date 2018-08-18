80dc1c130e9865c2db9ee0e95d039c9f26650918
Nathan Rajlich <nathan@tootallnate.net>
2018-08-18 01:32:45 -0700

    Fix absolute URLs

--- import.sh
+++ import.sh
@@ -8,7 +8,7 @@ import() {
   local cache="${IMPORT_CACHE-${HOME}/.import-cache}"
 
   # Apply the default server if the user is doing an implicit import
-  if ! echo "$url" | awk -F/ '{print $1}' | grep '\.' > /dev/null; then
+  if ! echo "$url" | grep "://" > /dev/null && ! echo "$url" | awk -F/ '{print $1}' | grep '\.' > /dev/null; then
     url="${IMPORT_SERVER-import.pw}/$url"
   fi
 
